aeab3e1b3d9113917c537103212dbf1c9528d77e
Zitu Hossain <zituhossain@gmail.com>
2022-11-10 01:51:01 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -143,7 +143,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://devzitu.netlify.app/src/assets/resume/zitu.pdf"
+              href="https://drive.google.com/file/d/1JK6gQjQWy4swuCspHJ5H1oLBfGSVo7x0/view?usp=share_link"
               target="_blank"
             >
               Resume <BsFillPersonLinesFill size={30} />
