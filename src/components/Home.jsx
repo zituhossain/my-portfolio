92ce63ba6329743a2a87e09a76fee959c98794e5
Zitu Hossain <zituhossain@gmail.com>
2024-05-28 19:59:22 +0600

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] h-full mx-auto px-8 flex flex-col justify-center">
-        <p className="text-pink-600">Hi, my name is</p>
+        <p className="text-pink-600">Hi, I am</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Zitu Hossain
         </h1>
