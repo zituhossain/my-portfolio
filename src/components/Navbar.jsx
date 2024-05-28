195fe3f9e4c6458295fedd018ec51f100835a2cb
Zitu Hossain <zituhossain@gmail.com>
2024-05-28 20:07:39 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -143,7 +143,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1ucInvQv_NgP3ZhqOxw914UnYq5qujw-B/view?usp=share_link"
+              href="https://drive.google.com/file/d/1bjXFbcuZAtC7tOh0_OzQrO_pUukb_eVa/view?usp=sharing"
               target="_blank"
             >
               Resume <BsFillPersonLinesFill size={30} />
